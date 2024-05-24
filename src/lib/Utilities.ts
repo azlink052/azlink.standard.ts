64a90ba5536b63ba4518d817df5bd1f974795c92
AZLINK.Norio Murata <nori@azlink.jp>
2024-05-24 18:29:47 +0900

    smooth scroll のfocus修正
    アンカーリンク後のfocus(isFocus: true)の場合はhtml側で移動要素に「tabindex: -1」をつける。
    ※アンカー要素であれば不要

--- src/lib/Utilities.ts
+++ src/lib/Utilities.ts
@@ -517,7 +517,6 @@ export class Utilities {
                 //     easing: easing,
                 //   });
                 // }
-                target.setAttribute('tabindex', '-1');
                 target.focus();
               }
             },
